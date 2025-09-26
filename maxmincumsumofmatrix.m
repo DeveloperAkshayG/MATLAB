b12a3d3f41598f2d8d21ede037e63884e4c6d629
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-26 22:54:52 +0530

    Update maxmincumsumofmatrix.m

--- maxmincumsumofmatrix.m
+++ maxmincumsumofmatrix.m
@@ -1,6 +1,6 @@
-%max min and cumsum for a matrix
+% max min and cumsum for a matrix
 
-%%section1 : creating matrix using randi function
+%% section1 : creating matrix using randi function
 balance=randi([1,10],2,4)
 
 minvalue=min(balance)
