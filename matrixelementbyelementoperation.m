fe2c71e8eaa0c1f9939775f4b12964a979a8231b
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-26 16:49:43 +0530

    Add files via upload

---
+++ matrixelementbyelementoperation.m
@@ -0,0 +1,14 @@
+%%matrix element by element operation
+a=[2:2:10]
+b=[5:2:13]
+a .* b
+
+%%matrix element by element exponentation
+c= [2 3 4]
+d= [2 2 2]
+c .^ d
+
+%%matrix element by element division
+e=[8 16 15]
+f=[2 4 3]
+res=e ./ f
