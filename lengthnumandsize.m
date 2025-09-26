66526f1f1c90d34e4cc09f57eeebcaa9a2cc2842
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-26 22:52:31 +0530

    Update lengthnumandsize.m

--- lengthnumandsize.m
+++ lengthnumandsize.m
@@ -1,11 +1,11 @@
-%length , numel and size of matrix
+% length , numel and size of matrix
 A = [1 2 4 5;5 10 15 20]
 
-%%LENGTH OF MATRIX
+% LENGTH OF MATRIX
 len=length(A)
 
-%%NUMEL OF MATRIX
+% NUMEL OF MATRIX
 num=numel(A)
 
-%%size of matrix
+% size of matrix
 [r c] = size(A)
