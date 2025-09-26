ba3f284ccd50f041224ca6a915b670fb1b0d3eaa
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-26 22:53:57 +0530

    Update matrixmultiplicationandvectoroperationexample.m

--- matrixmultiplicationandvectoroperationexample.m
+++ matrixmultiplicationandvectoroperationexample.m
@@ -6,16 +6,16 @@ A=[1.3 1.12]
 B=[-1.19 0.06]
 C=[0.54 0]
 
-%%position vector 
+% position vector 
 rA = A-C
 rB = B-C
 
-%%UNIT VECTOR = POSITION VECTOR/MAGNITUDE OF POSITION
+% UNIT VECTOR = POSITION VECTOR/MAGNITUDE OF POSITION
 uA = (rA) / norm(rA)
 uB = (rB) / norm(rB)
 
 FA = 500 * (uA)
 FB = 250 * (uB)
 
-%%projection of line
+% projection of line
 projection = dot(A,B)
