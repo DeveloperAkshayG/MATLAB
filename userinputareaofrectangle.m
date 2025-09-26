1ec63cfc72dcd0809d54fb4e0503271f77ba25e4
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-27 00:00:00 +0530

    user input example 1 in matlab

---
+++ userinputareaofrectangle.m
@@ -0,0 +1,9 @@
+% This script calculate area of rectangle with given user-defined base and height
+
+base=input('Enter the base of rectangle in inches: ')
+height=input('Enter the height of rectangle in inches: ')
+Name=input('Enter the name of rectangle: ','s')
+Area=base * height;
+
+% to display the area use "disp" function
+disp(Area)
