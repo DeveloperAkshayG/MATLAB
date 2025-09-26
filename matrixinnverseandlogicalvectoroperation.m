0643b31ab2129bc3e4976303dc128fb2577954bf
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-26 22:53:30 +0530

    Update matrixinnverseandlogicalvectoroperation.m

--- matrixinnverseandlogicalvectoroperation.m
+++ matrixinnverseandlogicalvectoroperation.m
@@ -1,17 +1,17 @@
-%%matrix equation solving
+% matrix equation solving
 a=[3 2 -1;-1 3 2;1 -1 -1]
 
 b=[10;5;-1]
 
-%%solving equation using x=a^-1*b
+% solving equation using x=a^-1*b
 c=a^-1 * b
 
-%%logical operator
+% logical operator
 vec=randi([-20,20],1,5)
 test=vec<0
 
-%%to find the element which satisfies test condition use "vector_name(resultofcondition)"
+% to find the element which satisfies test condition use "vector_name(resultofcondition)"
 negative=vec(test)
 
-%%to find location of negatives use "find(condition)" function
+% to find location of negatives use "find(condition)" function
 location=find(test)
