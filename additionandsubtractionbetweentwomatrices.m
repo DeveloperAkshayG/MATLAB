a6ec82c716f4bc2d0641dbb7723259d94dab7c3b
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-26 22:51:35 +0530

    Update additionandsubtractionbetweentwomatrices.m

--- additionandsubtractionbetweentwomatrices.m
+++ additionandsubtractionbetweentwomatrices.m
@@ -1,9 +1,9 @@
-%Addition and subtraction between two matrices
+% Addition and subtraction between two matrices
 A=randi([1,5],2,2)
 B=randi([2,5],2,2)
 
-%%ADDITION BETWEEN TWO MATRICES
+% ADDITION BETWEEN TWO MATRICES
 A+B
 
-%%SUBTRACTION BETWEEN TWO MATRICES
+% SUBTRACTION BETWEEN TWO MATRICES
 A-B
