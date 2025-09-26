9cb58bd11b9c8a8c2319efdef948f954eaaf23e4
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-26 22:54:28 +0530

    Update matrixmultiplicationdotcrossmagnitude.m

--- matrixmultiplicationdotcrossmagnitude.m
+++ matrixmultiplicationdotcrossmagnitude.m
@@ -1,18 +1,18 @@
-%matrix multiplication with a=2x3 matrix and b=3x2 matrix
+% matrix multiplication with a=2x3 matrix and b=3x2 matrix
 a=[2 4 6; 4 8 12]
 b=[3 6;5 10;12 2]
 c=a * b
 
-%%dot product of vector
+% dot product of vector
 a=[2:2:6]
 b=[5:5:15]
 c=dot(a,b)
 
-%%cross product of vector
+% cross product of vector
 a=[1 -4 5]
 b=[5 -8 3]
 c=cross(a,b)
 
-%%magnitude of a vector
+% magnitude of a vector
 line=[2 4 7]
 dist=norm(line)
