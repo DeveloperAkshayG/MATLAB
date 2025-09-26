6d31d39e1d17cf36a39c0d8d3a233b3fec025d2a
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-26 22:55:09 +0530

    Update quiz5chapter2udemy.m

--- quiz5chapter2udemy.m
+++ quiz5chapter2udemy.m
@@ -1,10 +1,10 @@
-%%ques 1
+%% ques 1
 A=[1 -0.5 -0.333;0 -0.707 0.667;0 0.5 0.667]
 B=[0;0;981]
 C=A^-1*B
 
-%%QUES2
+%% QUES2
 vec=[10 -2 13 -5 23 17 0]
 
-%%to find location for all the vectors greater than 5
+% to find location for all the vectors greater than 5
 location=find(vec>5)
