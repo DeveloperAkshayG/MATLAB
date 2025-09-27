a22c250a560e9f42d04cfbaae38177e4261c168e
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-28 00:49:16 +0530

    fprintf example exercise

---
+++ formattedprintexampleexcercise.m
@@ -0,0 +1,21 @@
+% formatted print statment example refer chapter 3 example pdf
+fprintf('%.0f\n',pi)
+pause
+fprintf('%.7f\n',pi)
+
+%%
+fprintf('%.5f\n',sqrt(786))
+fprintf('%20.10f\n',sqrt(786))
+fprintf('%.d\n',round(sqrt(786)))
+
+%%
+fprintf('The array is %dx%10d. \n',2,3)
+
+%%
+fprintf('%s\n','It is Tuesday')
+
+%%
+fprintf('%s\t','Hello')
+
+%%
+fprintf('%-10.2f',pi)
