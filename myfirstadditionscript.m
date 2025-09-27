b915f6f7f0ede926492b0d76485dca01de665c91
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-28 01:02:42 +0530

    taking input and display via fprintf

---
+++ myfirstadditionscript.m
@@ -0,0 +1,5 @@
+% this script demonstrate the use of input and displaying the output only using fprintf
+a=input('enter first number: ')
+b=input('enter second number: ')
+c=a+b;
+fprintf('The addition of %d and %d is %d\n',a,b,c)
