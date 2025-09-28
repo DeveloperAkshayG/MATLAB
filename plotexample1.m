6938953ca8717e8bb5ad2808fd2fcb4c0ed64d9c
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-28 12:30:16 +0530

    plot example 1 with cyant dash dot and sqaure data marker

---
+++ plotexample1.m
@@ -0,0 +1,6 @@
+% plot example with c-cyant -.:dash dot line,s-square data marker
+x= 0 :pi/40:4*pi;
+plot(x,sin(x),'c-.s')
+
+% plot with magenta dotted line with left triangle marker
+% plot(x,sin(x),'m:<')
