30554a9c01a96f63da61976a44ccecce33ab6945
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-28 14:34:06 +0530

    plot label and title with axis specified

---
+++ plotlabelandtitle2withaxisspecified.m
@@ -0,0 +1,10 @@
+% plot example with c-cyant -.:dash dot line,s-square data marker
+x= 0 :pi/40:4*pi;
+plot(x,sin(x),'c-.s')
+
+%% plot with magenta dotted line with left triangle marker
+plot(x,sin(x),'y-h')
+xlabel('Angle (Rad)')
+ylabel('sin (x)')
+title('sine function for value between 0 to 4*pi')
+axis([-1 16 -2 2])
