ff07ec2d2436518dd18acac731ed19287ab3e4fc
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-28 23:56:42 +0530

    two plot on same graph

---
+++ twoplotonsamegraph.m
@@ -0,0 +1,16 @@
+% plot example with c-cyant -.:dash dot line,s-square data marker
+x= 0 :pi/40:4*pi;
+plot(x,sin(x),'c-.s')
+
+%% plot with magenta dotted line with left triangle marker
+plot(x,sin(x),'m:<')
+xlabel('Angle (Rad)')
+ylabel('sin (x)')
+title('sine function for value between 0 to 4*pi')
+axis([-1 16 -2 2])
+hold on
+plot(x,cos(x),'g--*')
+ylabel('trig function')
+legend('sin(x)','cos(x)')
+grid on
+hold off
