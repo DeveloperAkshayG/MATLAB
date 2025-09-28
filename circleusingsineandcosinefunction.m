417dc9e40fc0eee4f8111990657d92d3be4ad21a
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-28 23:51:00 +0530

    circle using sine and cosine

---
+++ circleusingsineandcosinefunction.m
@@ -0,0 +1,6 @@
+% plot to which generates circle in elliptical shape which is fixed using axis equal
+x=0:pi/40:2*pi;
+plot (sin(x),cos(x))
+
+%using axis equal the shape becomes circular
+axis equal
