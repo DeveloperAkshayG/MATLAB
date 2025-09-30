c5dfc366fb09a54ec4bca919defbde1f1f392a72
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-09-30 16:52:34 +0530

    Update menuwithswitchcase.m

--- menuwithswitchcase.m
+++ menuwithswitchcase.m
@@ -3,11 +3,11 @@ x=input('enter the value for x: ')
 choice = menu('triag value','sin(x)','cos(x)','tan(x)')
 switch choice
     case 1
-        fprintf('sine (%.1f) is %.1f',x,sin(x))
+        fprintf('sine (%.1f) is %.1f\n',x,sin(x))
     case 2
-        fprintf('sine (%.1f) is %.1f',x,cos(x))
+        fprintf('sine (%.1f) is %.1f\n',x,cos(x))
     case 3
-        fprintf('sine (%.1f) is %.1f',x,tan(x))
+        fprintf('sine (%.1f) is %.1f\n',x,tan(x))
     otherwise
             disp('Error')
 end
