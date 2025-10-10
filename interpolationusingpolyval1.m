09a22e2d8e27a514adb73f006f1de232b41b4a73
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-10-10 13:02:39 +0530

    interpolation using polyval1

---
+++ interpolationusingpolyval1.m
@@ -0,0 +1,13 @@
+x=[124 126 128 130 140];
+y=[1.35 1.42 1.52 1.6 1.76];
+coeffs=polyfit(x,y,3);
+
+xi=[125 126.7 129.34];
+y_interp=polyval(coeffs,xi);
+
+plot(x,y,'bs-')
+hold on
+plot(xi,y_interp,'rp-')
+legend('raw data','interpolated data')
+
+
